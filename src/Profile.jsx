419c913b522462fdb84cbbd3601a34809a7044ad
Lis Lisovski <lis.lisovski2016@gmail.com>
2021-03-01 09:30:44 +0200

    Added info

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
          <div className="row justify-content-center ">
             <div className="col-8">
                <div className="card mb-3">
-                  <img src="./src/images/fox.jpg" className="card-img-top" alt="fox image"/>
+                  <img src="#" className="card-img-top" alt="fox image"/>
                   <div className="card-body">
                      <h5 className="card-title">Card title</h5>
                      <p className="card-text pb-2">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
